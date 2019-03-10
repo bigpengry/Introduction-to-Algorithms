97a810a730d575ee352e7206d0418c7a30bc0f87
bigpengry <bigpengry@163.com>
2019-03-10 12:45:50 +0800

    version 1.0

---
+++ chapter 2/2.1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func insertionSorting(a []int)[]int{
+	//注意课本上的数组下标是从1开始，而实际实现中是从0开始
+	for j:=1;j<len(a);j++ {
+		key:=a[j]
+		i:=j-1
+		//此处同上，注意跳出循环的边界值
+		for i>=0&&a[i]>key  {
+			a[i+1]=a[i]
+			i--
+		}
+		a[i+1]=key
+	}
+	return a
+}
+
+func main() {
+	a:=[]int{5,2,4,6,1,3}
+	fmt.Println(insertionSorting(a))
+}

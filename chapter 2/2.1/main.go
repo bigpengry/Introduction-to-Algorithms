fe3906495401494c1c8ec3d79c35a601e00ae305
bigpengry <bigpengry@163.com>
2019-03-24 19:49:07 +0800

    version1.1

--- chapter 2/2.1/main.go
+++ chapter 2/2.1/main.go
@@ -1,23 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func InsertionSort(a []int)[]int{
+func InsertionSort(A *[]int,start,end int){
 	//注意课本上的数组下标是从1开始，而实际实现中是从0开始
-	for j:=1;j<len(a);j++ {
-		key:=a[j]
+	for j:=start+1;j<end;j++ {
+		key:=(*A)[j]
 		i:=j-1
 		//此处同上，注意跳出循环的边界值
-		for i>=0&&a[i]>key  {
-			a[i+1]=a[i]
+		for i>=start&&(*A)[i]>key  {
+			(*A)[i+1]=(*A)[i]
 			i--
 		}
-		a[i+1]=key
+		(*A)[i+1]=key
 	}
-	return a
+}
+
+func init(){
+	//以时间作为初始化种子
+	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
-	a := []int{5, 2, 4, 6, 1, 3}
-	fmt.Println(InsertionSort(a))
+	n:=1000
+	A:=make([]int,n)
+	B:=make([]int,n)
+	for i:=0;i<n ;i++  {
+		A[i]=rand.Intn(n)
+		B[i]=rand.Intn(n)
+	}
+	m:=make(map[int]int)
+	for _,v:=range A {
+		m[v]=m[v]+1
+	}
+	fmt.Println(m)
+
+	fmt.Println(A)
+	InsertionSort(&A,0,len(A))
+	fmt.Println(A)
 }
